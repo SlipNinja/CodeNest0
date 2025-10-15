bdd74abce2e42b29062dc7b377b860f160832aa8
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-15 16:40:52 +0200

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { Menu } from './menu/menu';
+import { Footer } from './footer/footer';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Home, Menu],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+	selector: 'app-root',
+	imports: [RouterOutlet, Home, Menu, Footer],
+	templateUrl: './app.html',
+	styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('CodeNest0');
+	protected readonly title = signal('CodeNest0');
 }
