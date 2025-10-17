9e98068868d8f492ef1c6f56f289d592bc3239cf
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-17 16:47:25 +0200

    Added sign in form

--- src/app/signin/signin.ts
+++ src/app/signin/signin.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-signin',
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: './signin.html',
 	styleUrl: './signin.scss',
 })
-export class SignIn {}
+export class SignIn {
+	signInForm = new FormGroup({
+		username: new FormControl(''),
+		password: new FormControl(''),
+	});
+
+	submitSignIn(): void {
+		console.log(this.signInForm.value.username);
+		console.log(this.signInForm.value.password);
+	}
+
+	togglePassword(e: any) {
+		console.log(e.originalTarget.previousElementSibling);
+		const button = e.originalTarget;
+		const password = button.previousElementSibling;
+
+		if (password.type == 'text') {
+			button.classList = 'bi bi-eye';
+			password.type = 'password';
+		} else {
+			button.classList = 'bi bi-eye-slash';
+			password.type = 'text';
+		}
+	}
+}
