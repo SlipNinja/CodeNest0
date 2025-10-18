513b5a7b6c1151cd8f9ea532db1d506c75fb9ecb
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-18 12:58:01 +0200

    added sign-up form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { SignIn } from './signin/signin';
+import { SignUp } from './sign-up/sign-up';
 
 export const routes: Routes = [
 	{
 		path: 'sign-in',
 		component: SignIn,
 		title: 'Sign-in page',
+	},
+	{
+		path: 'sign-up',
+		component: SignUp,
+		title: 'Sign-up page',
 	},
 	{
 		path: '',
--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-	selector: 'app-signin',
+	selector: 'app-sign-up',
 	imports: [ReactiveFormsModule],
-	templateUrl: './signin.html',
-	styleUrl: './signin.scss',
+	templateUrl: './sign-up.html',
+	styleUrl: './sign-up.scss',
 })
-export class SignIn {
-	sign_in_form = new FormGroup({
+export class SignUp {
+	sign_up_form = new FormGroup({
 		username: new FormControl(''),
+		email: new FormControl(''),
 		password: new FormControl(''),
+		confirm_password: new FormControl(''),
 	});
 
 	submitSignIn(): void {
-		console.log(this.sign_in_form.value.username);
-		console.log(this.sign_in_form.value.password);
+		console.log(this.sign_up_form.value.username);
+		console.log(this.sign_up_form.value.password);
 	}
 
 	togglePassword(e: any) {
