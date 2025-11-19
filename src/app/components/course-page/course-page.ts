67de3c2f3e3a92ce263d16f20143f7fc961f4090
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-11-19 14:22:34 +0100

    add course page header + style code editor

--- src/app/components/course-page/course-page.ts
+++ src/app/components/course-page/course-page.ts
@@ -1,6 +1,10 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { basicSetup } from 'codemirror';
+import { EditorView } from '@codemirror/view';
+import { DataParser } from '@services/data-parser';
+import { CourseInfo } from '@interfaces/course-info';
 
 @Component({
 	selector: 'app-course-page',
@@ -11,11 +15,37 @@ import { Subscription } from 'rxjs';
 export class CoursePage implements OnInit, OnDestroy {
 	private route_sub: Subscription;
 	private readonly route = inject(ActivatedRoute);
+	view: EditorView;
+	step_quantity: boolean[] = [true, false, false, false, false]; // TO CHANGE
+	parser: DataParser = inject(DataParser);
+	current_course: CourseInfo;
 
 	ngOnInit(): void {
 		this.route_sub = this.route.params.subscribe((params) => {
 			console.log('Loading course with id', params['id']);
+			this.parser.fetchCourse(params['id']).then((course) => {
+				this.current_course = course;
+			});
 		});
+
+		this.view = new EditorView({
+			extensions: [basicSetup],
+		});
+
+		document.querySelector('#code_zone')?.appendChild(this.view.dom);
+
+		this.add_lines(10);
+	}
+
+	get_code() {
+		return this.view.state.doc.toString();
+	}
+
+	add_lines(n: number) {
+		let transaction = this.view.state.update({
+			changes: { from: 0, insert: '\n'.repeat(n) },
+		});
+		this.view.dispatch(transaction);
 	}
 
 	ngOnDestroy() {
