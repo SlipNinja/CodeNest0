92ef9a4181611a14732091e031608990967e5628
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-11-24 16:59:13 +0100

    feat(course-page): add basic multi-testing to iframe

--- public/assets/test/test.js
+++ public/assets/test/test.js
@@ -1,8 +1,39 @@
 window.addEventListener('message', (e) => {
-	console.log('DATA RECEIVED :');
-	console.log(e.data);
+	const data = e.data;
+	const code = data['code'];
+	const tests = data['tests'];
+	const f_name = data['f_name'];
 
-	// https://stackoverflow.com/questions/42309058/using-javascripts-eval-with-multiple-dependent-functions
+	const results = [];
 
-	window.top.postMessage('RESULTS', '*');
+	for (const test of tests) {
+		// Result of the current test
+		const r = { output: undefined, passed: false, result: '' };
+
+		// Fonction call
+		const f_return = `return ${f_name}(${test['f_params'].join(',')});`;
+		const f_call = `${code}${f_return}`;
+
+		try {
+			r['output'] = new Function(f_call)();
+		} catch (e) {
+			r['output'] = undefined;
+			r['result'] = `Error : ${e.message}`;
+			r['passed'] = false;
+			continue;
+		}
+
+		if (r['output'] == test['f_result']) {
+			r['result'] = `${f_name}(${test['f_params'].join(',')}) = ${r['output']}`;
+			r['passed'] = true;
+		} else {
+			r['result'] = `${f_name}(${test['f_params'].join(',')}) != ${r['output']}`;
+			r['passed'] = false;
+		}
+
+		results.push(r);
+	}
+
+	document.body.textContent = JSON.stringify(results);
+	window.top.postMessage(result, '*');
 });
--- src/app/components/course-page/course-page.ts
+++ src/app/components/course-page/course-page.ts
@@ -17,6 +17,9 @@ export class CoursePage implements OnInit, OnDestroy {
 	private route_sub: Subscription;
 	private readonly route = inject(ActivatedRoute);
 	view: EditorView;
+	parser: DataParser = inject(DataParser);
+	current_course: CourseInfo;
+
 	step_quantity = [
 		{ step_number: 1, done: true },
 		{ step_number: 2, done: true },
@@ -24,12 +27,28 @@ export class CoursePage implements OnInit, OnDestroy {
 		{ step_number: 4, done: false },
 		{ step_number: 5, done: false },
 	]; // TO CHANGE
-	parser: DataParser = inject(DataParser);
-	current_course: CourseInfo;
+
+	example_exercice: any = {
+		lang: javascript(),
+		f_name: 'multiply',
+		tests: [
+			{
+				f_params: [5, 5],
+				f_result: 25,
+			},
+			{
+				f_params: [5, 0],
+				f_result: 0,
+			},
+			{
+				f_params: [234, 12],
+				f_result: 2808,
+			},
+		],
+	};
 
 	ngOnInit(): void {
 		this.route_sub = this.route.params.subscribe((params) => {
-			console.log('Loading course with id', params['id']);
 			this.parser.fetchCourse(params['id']).then((course) => {
 				this.current_course = course;
 			});
@@ -38,32 +57,44 @@ export class CoursePage implements OnInit, OnDestroy {
 		this.init_editor();
 
 		window.addEventListener('message', (e) => {
-			console.log(e.data);
+			const results = JSON.parse(e.data);
+			console.log(results);
 		});
 
+		const data = {
+			f_name: this.example_exercice['f_name'],
+			tests: this.example_exercice['tests'],
+			code: this.get_code(),
+		};
+
 		document.getElementById('run_button')?.addEventListener('click', (e) => {
-			this.send_iframe(this.get_code());
+			this.send_iframe(data);
 		});
 	}
 
 	init_editor() {
 		this.view = new EditorView({
-			extensions: [basicSetup, javascript()],
+			extensions: [basicSetup, this.example_exercice['lang']],
 		});
 
 		document.querySelector('#code_zone')?.appendChild(this.view.dom);
 		this.add_lines(10);
 
 		let transaction = this.view.state.update({
-			changes: { from: 0, insert: 'console.log("COUCOU");\n//J\'aime les patates\n' },
+			changes: {
+				from: 0,
+				insert: "let abc = 123;\n//J'aime les patates\nfunction please_work(a, b){\n\treturn a*b;\n}\nfunction multiply(x, y){\n\treturn please_work(y,x);\n}",
+			},
 		});
 		this.view.dispatch(transaction);
 	}
 
-	send_iframe(data: string) {
+	send_iframe(data: object) {
 		const game_frame = document.getElementsByTagName('iframe')[0];
-		const iframe_origin = new URL(game_frame.src).origin;
-		console.log(iframe_origin);
+
+		// const iframe_origin = new URL(game_frame.src).origin;
+		// console.log(iframe_origin);
+
 		game_frame.contentWindow?.postMessage(data, '*');
 	}
 
