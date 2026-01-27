2357a4bcf1c3ff786b386ac57fb08fd3693c3946
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2026-01-27 16:44:08 +0100

    Feat(menu): updates when user connexion status change

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserHandler } from '@services/user-handler';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
 	selector: 'app-menu',
@@ -11,7 +12,15 @@ import { UserHandler } from '@services/user-handler';
 export class Menu {
 	connected: boolean = false;
 
-	constructor(private readonly user_handler: UserHandler) {
-		user_handler.connected.subscribe((state) => (this.connected = state));
+	constructor(
+		private readonly user_handler: UserHandler,
+		private readonly cookie_service: CookieService,
+	) {
+		// If user is already connected
+		const jwt_token = this.cookie_service.get('jwt_token');
+		if (jwt_token) this.connected = true;
+
+		// Listen to connexion state to update
+		this.user_handler.connected.subscribe((state) => (this.connected = state));
 	}
 }
