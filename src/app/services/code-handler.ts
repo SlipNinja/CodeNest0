c52d1092b98878664b85c95362c1c579bbc328ec
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2026-02-17 18:13:19 +0100

    clean: remove unused import

--- src/app/services/code-handler.ts
+++ src/app/services/code-handler.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Step } from '@interfaces/step';
 import { TestResponse } from '@interfaces/test-response';
 
